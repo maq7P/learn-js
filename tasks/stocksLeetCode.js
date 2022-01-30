991685c8db2e0efad4a144dde5a33b69befb51ed
maq7 <maxpet16@mail.ru>
2022-01-30 22:34:20 +0500

    task[resolved simple stock task from leet code]

---
+++ tasks/stocksLeetCode.js
@@ -0,0 +1,33 @@
+/*
+#1 
+    There are array of stocks prices. It's necessary maximum pofit purchase-sale
+    - In one day there is an opportunity to buy or sell
+    - Length of array: 1 < prices.length <= 3*10⁴
+
+    input: [7,1,5,3,6,4]
+    output: 7
+
+    input: [7,6,5,4,3,2]
+    output: 0
+
+*/
+
+// memory O(1)
+// speed O(n)
+
+const exPrices1 = [7,1,5,3,6,4];
+const exPrices2 = [7,6,5,4,3,2];
+
+const maxProfit = (prices) => {
+    let prfit = 0;
+
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] > prices[i - 1]){
+            prfit += prices[i] - prices[i - 1]
+        }
+    }
+}
+maxProfit()
+
+/*
+#2
