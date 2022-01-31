aa1a2ea521ac0cfe02050ae8d418ac33d2e514fa
maq7 <maxpet16@mail.ru>
2022-01-31 19:34:42 +0500

    task[second task of stocks from leetcode]

--- tasks/stocksLeetCode.js
+++ tasks/stocksLeetCode.js
@@ -31,3 +31,38 @@ maxProfit()
 
 /*
 #2
+ (Task about extremum function)
+
+ There are array of stocks prices. It's necessary maximum pofit purchase-sale
+    - there is an opportunity to buy or sell on of period
+    - Length of array: 1 < prices.length <= 3*10⁴
+
+    input: [7,1,5,3,6,4]
+    output: 5
+
+    input: [7,6,5,4,3,2]
+    output: 0
+
+*/
+
+// memory O(1)
+// speed O(n)
+
+const exPrices1 = [7,1,5,3,6,4];
+const exPrices2 = [7,6,5,4,3,2];
+
+const maxProfitWithOnDeal = (prices) => {
+
+    let minPrice = prices[0];
+    let maxProfi = 0;
+    
+    for(let i = 0; i < prices.length; i++){
+        const cur = prices[i];
+        
+        minPrice < cur && (minPrice = cur);
+        
+        (maxProfit < cur - minPrice) && (maxProfit = cur - minPrice)
+    }
+
+    return maxProfi
+}
