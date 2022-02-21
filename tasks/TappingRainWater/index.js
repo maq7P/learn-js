95e9cfae71e3cd7396f3f104934a6752c286e1ed
maq7 <maxpet16@mail.ru>
2022-02-21 23:40:24 +0500

    task[have been resolved task of tapping rain water]

---
+++ tasks/TappingRainWater/index.js
@@ -0,0 +1,45 @@
+const pillars = [0,1,0,2,1,0,1,3,2,1,2,1];
+
+
+//complexity time O(N)
+//complexity memory O(N)
+const tappingRainWater = (pillars) => {
+    const maxLeft = {};
+    const maxRight = {};
+    const pillarInfo = {
+        source: {},
+        maxLeft: {},
+        maxRight: {},
+        volume: {}
+    }
+    let temp = 0;
+    for(let i = 0; i < pillars.length; i++){
+        const cur = pillars[i];
+        pillarInfo.source[i + '-' + cur] = cur;
+        pillarInfo.maxLeft[i + '-' + cur] = temp;
+        temp < cur && (temp = cur)
+    }
+    temp = 0;
+
+    for(let i = pillars.length - 1; i > 0; i--){
+        const cur = pillars[i];
+
+        pillarInfo.maxRight[i + '-' + cur] = temp;
+        temp < cur && (temp = cur)
+    }
+
+    for(key in pillarInfo.source){
+        const curMin = Math.min(pillarInfo.maxLeft[key], pillarInfo.maxRight[key]) - pillarInfo.source[key];
+        pillarInfo.volume[key] = curMin > 0 ? curMin : 0;
+    }
+
+    let res = 0;
+    for(key in pillarInfo.volume){
+        res += pillarInfo.volume[key];
+    }
+
+    return res;
+}
+
+console.log(tappingRainWater(pillars));
+
---
+++ tasks/TappingRainWater/readme.md
@@ -0,0 +1,16 @@
+# Собрать додевую воду
+
+### Условие
+
+According to the conditions: we have an array with the height of the relief as input. 
+Imagine that each "cell" of the relief has a width 
+of 1 and a height of the one that is specified in a particular element of the array. 
+Our task is to calculate how many elements (units) of water can accumulate 
+in such a relief if it rains.
+
+Of the additional conditions, this is that the length of the array 
+can be from 1 to 10,000, and the values ​​of the elements in the array can be from 0 to 100,000.
+
+### Алгоритм решения визуально
+![](../../source/rain.jpg)
+
