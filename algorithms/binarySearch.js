89117453d4324f78abb8912f1994fcff711f0977
maq7 <maxpet16@mail.ru>
2022-04-30 22:35:23 +0500

    [alg]: have been added task from hexlet for binarySearch

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -55,3 +55,47 @@ const binarySearchRecursive2 = (nums, target, start = 0, end = nums.length - 1) 
 }
 
 console.log(binarySearchRecursive2([1,2,3,4,50,60,70, 1004, 2000], 60));
+
+
+
+
+//По имени вывести номер с помощью бинарного поиска
+const d = [
+    { name: 'Alex Bowman', number: '48-2002' },
+    { name: 'Aric Almirola', number: '10-1001' },
+    { name: 'Bubba Wallace', number: '23-1111' },
+    { name: 'Chase Elliott', number: '9-9900' },
+    { name: 'Denny Hamlin', number: '11-0022' },
+    { name: 'John Logano', number: '22-2299' },
+    { name: 'Kevin Harvick', number: '4-1154' },
+    { name: 'Kyle Busch', number: '18-3002' },
+    { name: 'Kyle Larson', number: '5-2234' },
+    { name: 'Matt Dibenedetto', number: '21-1102' }
+];
+
+function findNumberByName(phonebook, target) {
+    let left = 0;
+    let right = phonebook.length - 1;
+
+    while (left <= right) {
+        const mid = Math.round((right - left) / 2) + left;
+        const curName = phonebook[mid].name.toLowerCase();
+        const curNum = phonebook[mid].number;
+
+        if (curName === target.toLowerCase()) {
+            return curNum
+        }
+
+        if (target.toLowerCase() > curName) {
+            left = mid + 1
+        }
+        if (target.toLowerCase() < curName){
+            right = mid - 1
+        }
+
+    }
+
+    return null
+}
+
+console.log(findNumberByName(d, 'Chase Elliott'))
