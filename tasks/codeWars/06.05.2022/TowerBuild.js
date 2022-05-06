99254ad510457b0755bc2ca61dc29c4b8b98d1c9
maq7 <maxpet16@mail.ru>
2022-05-06 23:57:41 +0500

    [task]: have been resolved task tower build

--- tasks/codeWars/06.05.2022/TowerBuild.js
+++ tasks/codeWars/06.05.2022/TowerBuild.js
@@ -5,11 +5,16 @@ function towerBuilder(n) {
 	let temp = n - 1;
 	let res = [];
 	for(let i = 0; i < n; i++){
-		res.push(temp * EMPTY + BRICK + temp * EMPTY);
+		res.push(EMPTY.repeat(temp) + BRICK.repeat((i * 2) + 1) + EMPTY.repeat(temp));
 		temp--;
 	}
 
 	return res
 }
 
-console.log(towerBuilder(3))
+
+//однострочник
+const towerBuilder2 = n =>
+		[...Array(n)].map((_, i) => ' '.repeat(n-i-1) + '*'.repeat(i*2+1) + ' '.repeat(n-i-1))
+
+console.log(towerBuilder(10))
