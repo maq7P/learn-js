c30f0960dbc912d5b3a4957956c971571f350b4b
maq7 <maxpet16@mail.ru>
2022-06-17 15:03:14 +0500

    [task]: have been resolved task maxSubArray from leetcode

---
+++ tasks/leetcodePatterns/Easy/#7. MaximumSubarray/index.ts
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/maximum-subarray/
+
+function maxSubArray(nums: number[]): number {
+  let curSum = nums[0]
+  let sum = nums[0]
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    curSum = Math.max(curSum + num, num)
+    sum = Math.max(curSum, sum)
+  }
+
+  return sum
+}
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) //6 -> [4,-1,2,1]
