6750505dfb85c625cd82a25aec85da2dad585eb8
maq7 <maxpet16@mail.ru>
2022-06-27 21:22:01 +0500

    [task]: have been resolved removeElement from linked list

---
+++ tasks/leetcodePatterns/Easy/#14.RemoveLinkedListElements/index.js
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/remove-linked-list-elements/
+import ListNode from "../ListNode/index";
+function removeElements(head, val) {
+    var _a;
+    if (!head)
+        return null;
+    const result = new ListNode(0);
+    result.next = head;
+    let cur = result;
+    while (cur === null || cur === void 0 ? void 0 : cur.next) {
+        if (cur.next.val === val) {
+            cur.next = ((_a = cur.next) === null || _a === void 0 ? void 0 : _a.next) || null;
+        }
+        else {
+            cur = cur.next;
+        }
+    }
+    return result.next;
+}
---
+++ tasks/leetcodePatterns/Easy/#14.RemoveLinkedListElements/index.ts
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/remove-linked-list-elements/
+
+import ListNode from "../ListNode/index";
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  if(!head) return null
+
+  const result = new ListNode(0)
+  result.next = head
+
+  let cur: ListNode | null = result
+
+  while(cur?.next){
+    if(cur.next.val === val) {
+      cur.next = cur.next?.next || null
+    } else {
+      cur = cur.next
+    }
+  }
+
+  return result.next
+}
