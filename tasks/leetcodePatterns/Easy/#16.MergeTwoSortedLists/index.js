3f3d94160a45c316cbeae6c5bfe6556cb6464a48
maq7 <maxpet16@mail.ru>
2022-06-29 21:06:42 +0500

    [task]: have been resolved task merge sorted linked lists

---
+++ tasks/leetcodePatterns/Easy/#16.MergeTwoSortedLists/index.js
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/
+import ListNode from "../ListNode/index";
+function mergeTwoLists(list1, list2) {
+    let merged = new ListNode(0);
+    const res = merged;
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            merged.next = new ListNode(list1.val);
+            list1 = list1.next;
+        }
+        else {
+            merged.next = new ListNode(list2.val);
+            list2 = list2.next;
+        }
+        merged = merged.next;
+    }
+    while (list1) {
+        merged.next = new ListNode(list1.val);
+        list1 = list1.next;
+        merged = merged.next;
+    }
+    while (list2) {
+        merged.next = new ListNode(list2.val);
+        list2 = list2.next;
+        merged = merged.next;
+    }
+    return res.next;
+}
