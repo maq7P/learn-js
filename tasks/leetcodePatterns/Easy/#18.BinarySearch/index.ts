ecd4679709ab72134c9021411b0e23755ecd02c6
maq7 <maxpet16@mail.ru>
2022-07-01 19:29:31 +0500

    [task]: have been resolved binary search from leetcode Easy

---
+++ tasks/leetcodePatterns/Easy/#18.BinarySearch/index.ts
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/binary-search/
+
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1
+
+  while(right >= left){
+    let mid = Math.floor( (left + (right - left) / 2))
+
+    if(nums[mid] === target) return mid
+
+    if(nums[mid] < target){
+      left = mid + 1
+    } else right = mid - 1
+  }
+
+  return -1
+}
+
+console.log(search([1,2,3,4, 10], 4))
+console.log(search([1,2,3,4, 10, 100, 2000], 100))
+console.log(search( [-1,0,3,5,9,12], 2))
