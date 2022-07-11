9a9cd19489f185b7513f8bc559a971ffe83e14e7
maq7 <maxpet16@mail.ru>
2022-07-11 20:17:23 +0500

    [task]: have been added function decrypt for resolve prev kata

--- tasks/codeWars/_stream/10.07.2022/SimpleEncryptionAlternating Split.js
+++ tasks/codeWars/_stream/10.07.2022/SimpleEncryptionAlternating Split.js
@@ -1,19 +1,33 @@
 //https://www.codewars.com/kata/57814d79a56c88e3e0000786/train/javascript
 
 function encrypt(text, n) {
-	if(n <= 0) return text;
+	if(n <= 0 || !text) return text;
 
 	const first = text.split("").filter((item, idx) => idx % 2 === 1);
 	const second = text.split("").filter((item, idx) => idx % 2 === 0);
 
-	return first.concat(second).join("");
+	return encrypt(first.concat(second).join(""), n - 1);
 }
 
-//TODO: overthinking
 function decrypt(encryptedText, n) {
 	if(n <= 0 || !encryptedText) return encryptedText;
 
+	let res = [];
 
+	const mid = encryptedText.length / 2;
+	const first = encryptedText.split("").splice(mid);
+	const second = encryptedText.split("").splice(0, mid);
+
+	for (let i = 0; i < encryptedText.length; i++) {
+			res.push(first[i]);
+			res.push(second[i]);
+	}
+
+	return decrypt(res.join(""), n - 1)
 }
 
-console.log(decrypt(encrypt("0123456789")))
+console.log(decrypt("This is a test!", 0))
+console.log(decrypt("hsi  etTi sats!", 1))
+console.log(decrypt("012345", 1))
+console.log(decrypt("This is a test!", 4))
+console.log(decrypt("hsi  etTi sats!", 1))
