91c743517bea6ff1a5fcee6a7b5f896d59e81865
maq7 <maxpet16@mail.ru>
2022-07-18 20:28:43 +0500

    task form codewars 6kye with sort

--- tasks/codeWars/_stream/18.07.22/FindUniqueNumber.js
+++ tasks/codeWars/_stream/18.07.22/FindUniqueNumber.js
@@ -17,6 +17,12 @@ function findUniq(arr) {
 	return Object.values(res).filter(item => item.count === 1)[0].val
 }
 
+// use sort
+function findUniqSort(arr) {
+	arr.sort((a,b)=>a-b);
+	return arr[0]==arr[1]?arr.pop():arr[0]
+}
+
 console.log(findUniq([ 1, 0, 0 ]), 1);
 console.log(findUniq([ 0, 1, 0 ]), 1);
 console.log(findUniq([ 0, 0, 1 ]), 1);
