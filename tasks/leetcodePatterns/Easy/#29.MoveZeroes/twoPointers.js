757a2c309f657211b75771d69ae3a7bff844d61a
maq7 <maxpet16@mail.ru>
2022-10-09 23:10:52 +0500

    [leetCode]: have been resolved MoveZeroes with two pointer

---
+++ tasks/leetcodePatterns/Easy/#29.MoveZeroes/twoPointers.js
@@ -0,0 +1,20 @@
+"use strict";
+//https://leetcode.com/problems/move-zeroes/
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+const nums1 = [0, 1, 0, 3, 12];
+const nums2 = [1, 2];
+function moveZeroes2(nums) {
+    let temp = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            [nums[temp], nums[i]] = [nums[i], nums[temp]];
+            temp++;
+        }
+    }
+}
+moveZeroes2(nums1);
+moveZeroes2(nums2);
+console.log(nums1);
+console.log(nums2);
