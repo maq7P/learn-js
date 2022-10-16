f314d26c76b809f873b59d7cc1066bbe9ba55c7e
maq7 <maxpet16@mail.ru>
2022-10-17 01:21:41 +0500

    [leetCode]: have been resolved tribonacci recursive with cache

--- tasks/leetcodePatterns/Easy/#38.N-thTribonacciNumber/recursive.ts
+++ tasks/leetcodePatterns/Easy/#38.N-thTribonacciNumber/recursive.ts
@@ -1,9 +1,15 @@
+const cache: Record<number, number> = {}
+
 function tribonacciRecursive(n: number): number {
   if(n === 0) return 0
   if(n === 1) return 1
   if(n === 2) return 1
 
-  return tribonacciRecursive(n - 3) + tribonacciRecursive(n - 2) + tribonacciRecursive(n - 1)
+  const getWithCache = (n: number) => {
+    return cache[n] ? cache[n] : (cache[n] = tribonacciRecursive(n))
+  }
+  
+  return getWithCache(n - 3) + getWithCache(n - 2) + getWithCache(n - 1)
 };
 
 console.log(tribonacciRecursive(25));
