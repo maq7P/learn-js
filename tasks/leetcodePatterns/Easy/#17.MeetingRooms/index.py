697c48f09f3432ddfba9bdd6cd1af35717cd7b31
maq7 <maxpet16@mail.ru>
2022-10-16 16:42:43 +0500

    [leetCode]: have been resolved task of MeetingRooms with Python

---
+++ tasks/leetcodePatterns/Easy/#17.MeetingRooms/index.py
@@ -0,0 +1,20 @@
+def canAttendMeetings(intervals):
+  intervals.sort(key=lambda x:x["start"])
+
+  for i in range(len(intervals) - 1):
+    if intervals[i]["end"] > intervals[i + 1]["start"]:
+      return False
+
+  return True
+
+# False
+print(canAttendMeetings([
+  {"start": 5, "end": 15},
+  {"start": 11, "end": 20},
+]))
+
+# True
+print(canAttendMeetings([
+  {"start": 5, "end": 10},
+  {"start": 11, "end": 20},
+]))
