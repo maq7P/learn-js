fd50ba5354d9322a4326dbf5f85c3426d01413b9
maq7 <maxpet16@mail.ru>
2022-10-17 10:40:21 +0500

    [leetCode]: have been resolved Pascal's triangle with my way

---
+++ tasks/leetcodePatterns/Easy/#39.Pascal'sTriangle/index.ts
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/pascals-triangle/
+
+function generate(numRows: number): number[][] {
+  let triangle = [[1], [1,1]]
+
+  if(numRows === 1) return [triangle[0]]
+  if(numRows === 2) return triangle
+
+  for(let i = 3; i <= numRows; i++){
+    const prevLine = triangle[i - 2]
+    const middlePart = []
+
+    for(let j = 1; j < prevLine.length; j++){
+      const curSum = prevLine[j - 1] + prevLine[j]
+      middlePart.push(curSum)
+    }
+    triangle.push([1, ...middlePart, 1])
+  }
+
+  return triangle
+};
+
+console.log(generate(5));
