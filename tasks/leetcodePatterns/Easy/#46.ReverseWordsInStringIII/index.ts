551bd82ba0c539a73377a970b6372823b428ef08
maq7 <maxpet16@mail.ru>
2022-10-21 12:20:49 +0500

    [leetCode]: have been resolved Reverse words in string III

---
+++ tasks/leetcodePatterns/Easy/#46.ReverseWordsInStringIII/index.ts
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+function reverseWords(s: string): string {
+  const res = []
+
+  for(let c of s.split(" ")){
+    const arr = c.split("")
+    let left = 0, right = c.length - 1
+
+    while(left <= right){
+      [arr[left], arr[right]] = [arr[right], arr[left]]
+      left++
+      right--
+    }
+
+    res.push(arr.join(""))
+  }
+
+  return res.join(" ")
+};
+
+console.log(reverseWords("Let's take LeetCode contest"))
