a5a158c6510514c1a6f01ef3dc69fc538a408f98
maq7 <maxpet16@mail.ru>
2022-10-22 12:39:00 +0500

    [leetCode]: have been resolved task Guess Number Higher or Lower

---
+++ tasks/leetcodePatterns/Easy/#48.GuessNumberHigherOrLower/index.js
@@ -0,0 +1,35 @@
+"use strict";
+//https://leetcode.com/problems/guess-number-higher-or-lower/
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+const guess = (pick, guess = 5) => {
+    if (pick === guess)
+        return 0;
+    return pick > guess ? 1 : -1;
+};
+function guessNumber(n) {
+    let left = 0;
+    let right = n;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const guessCall = guess(mid);
+        if (guessCall === 1) {
+            left = mid + 1;
+            continue;
+        }
+        if (guessCall === -1) {
+            right = mid - 1;
+            continue;
+        }
+        return mid;
+    }
+    return left;
+}
+;
+console.log(guessNumber(10));
