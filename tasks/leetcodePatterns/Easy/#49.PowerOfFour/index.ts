d6349ef186952bd698bba3c6fdeda045820e46bb
maq7 <maxpet16@mail.ru>
2022-10-23 13:37:02 +0500

    [leetCode]: my solution with check length

--- tasks/leetcodePatterns/Easy/#49.PowerOfFour/index.ts
+++ tasks/leetcodePatterns/Easy/#49.PowerOfFour/index.ts
@@ -6,7 +6,8 @@
 // 1000 & (1000 - 0001) = 1000 & 0111 = 0 
 
 
-// faster than 35%
+// faster than 35% (O(1) O(1))
+
 function isPowerOfFour2(n: number): boolean {
   if(!(n > 0 && (n & (n - 1)) === 0)){
     return false
@@ -16,4 +17,14 @@ function isPowerOfFour2(n: number): boolean {
   return sq * sq === n
 };
 
-console.log(isPowerOfFour2(4));
+// faster than 35% (O(1) O(1))
+
+function isPowerOfFour3(n: number): boolean {
+  if(n === 1) return true
+    
+  if((n & (n - 1)) === 0 && ((n).toString(2).length) % 2 === 1 && n > 0){
+    return true
+  }
+    
+  return false
+};
