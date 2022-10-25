8e4daefc378a19155f624eb09c2d08034d86f6fa
maq7 <maxpet16@mail.ru>
2022-10-25 16:28:42 +0500

    [leetCode]: have been resolved can place flowers

--- tasks/leetcodePatterns/Easy/#52.CanPlaceFlowers/index.js
+++ tasks/leetcodePatterns/Easy/#52.CanPlaceFlowers/index.js
@@ -1,31 +1,18 @@
 "use strict";
 //https://leetcode.com/problems/can-place-flowers/
 function canPlaceFlowers(flowerbed, n) {
-    if (flowerbed.length === 1 && flowerbed[0] === 0)
-        return 1 === n;
-    let canBePlanted = 0, checker = [flowerbed[0]], i = 1;
-    if (flowerbed[0] === 0 && flowerbed[1] === 0) {
-        canBePlanted++;
-        checker[0] = 1;
-        i = 2;
-    }
-    for (; i < flowerbed.length; i++) {
-        if (checker[checker.length - 1] === 0
-            && flowerbed[i] === 0
-            && flowerbed[i + 1] !== 1) {
-            checker[checker.length - 1] = 1;
-            canBePlanted++;
-        }
-        else {
-            checker.push(flowerbed[i]);
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (((flowerbed[i - 1] || 0) + flowerbed[i] + (flowerbed[i + 1] || 0)) === 0) {
+            flowerbed[i] = 1;
+            n--;
         }
     }
-    return canBePlanted === n;
+    return n === 0;
 }
 ;
-console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1));
-console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2));
-console.log(canPlaceFlowers([1, 0, 0, 0, 0, 0, 1], 2));
-console.log(canPlaceFlowers([1, 0, 0, 0, 0, 1], 2));
-console.log(canPlaceFlowers([0, 0, 1, 0, 1], 1));
-console.log(canPlaceFlowers([0, 0, 1, 0, 0], 2));
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1)); // true
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2)); // false
+console.log(canPlaceFlowers([1, 0, 0, 0, 0, 0, 1], 2)); // true
+console.log(canPlaceFlowers([1, 0, 0, 0, 0, 1], 2)); // false
+console.log(canPlaceFlowers([0, 0, 1, 0, 1], 1)); // true
+console.log(canPlaceFlowers([0, 0, 1, 0, 0], 2)); // true
