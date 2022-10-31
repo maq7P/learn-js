0d28d95ca2ce31022241fd1bc8229d1e6758d439
maq7 <maxpet16@mail.ru>
2022-10-31 14:16:17 +0500

    [leetCode]: refactor flipping an image

--- tasks/leetcodePatterns/Easy/#61.FlippingAnImage/index.js
+++ tasks/leetcodePatterns/Easy/#61.FlippingAnImage/index.js
@@ -5,14 +5,16 @@ function flipAndInvertImage(image) {
         for (let j = 0; j < image[i].length; j++) {
             const left = j;
             const right = image[i].length - 1 - j;
-            image[i][j] = image[i][j] == 0 ? 1 : 0;
             if (left > right)
                 break;
-            [image[i][left], image[i][right]] = [image[i][right], image[i][left]];
+            [image[i][left], image[i][right]] = [
+                image[i][right] === 0 ? 1 : 0,
+                image[i][left] === 0 ? 1 : 0
+            ];
         }
         // for(let j = 0; j < image[i].length; j++){
         //   image[i][j]  = image[i][j] == 0 ? 1 : 0
-        // }
+        // } 
     }
     return image;
 }
--- tasks/leetcodePatterns/Easy/#61.FlippingAnImage/index.ts
+++ tasks/leetcodePatterns/Easy/#61.FlippingAnImage/index.ts
@@ -2,20 +2,17 @@
 
 function flipAndInvertImage(image: number[][]): number[][] {
   for(let i = 0; i < image.length; i++){
-
-
     for(let j = 0; j < image[i].length; j++){
       const left = j
       const right = image[i].length - 1 - j
       
       if(left > right) break;
       
-      [image[i][left], image[i][right]] = [image[i][right], image[i][left]]
+      [image[i][left], image[i][right]] = [
+        image[i][right] === 0 ? 1 : 0, 
+        image[i][left] === 0 ? 1 : 0
+      ]
     }
-
-    for(let j = 0; j < image[i].length; j++){
-      image[i][j]  = image[i][j] == 0 ? 1 : 0
-    } 
   }
 
   return image
