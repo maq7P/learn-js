4c9faf21db0814a86a28baed1464f2e3d6f31258
maq7 <maxpet16@mail.ru>
2022-11-02 14:46:36 +0500

    [leetCode]: have been resolved task verifying alien dictonary with my way

--- tasks/leetcodePatterns/Easy/#63.VerifyingAlienDictionary/index.js
+++ tasks/leetcodePatterns/Easy/#63.VerifyingAlienDictionary/index.js
@@ -1,23 +1,38 @@
 "use strict";
 //https://leetcode.com/problems/verifying-an-alien-dictionary/
 function isAlienSorted(words, order) {
-    const orderPrice = {};
+    if (words.length < 2)
+        return true;
+    const dictonary = {};
     for (let i = 0; i < order.length; i++) {
-        orderPrice[order[i]] = i;
+        dictonary[order[i]] = i;
     }
-    let prevPrice = 0;
-    for (let word of words) {
-        let curPrice = 0;
-        for (let char of word) {
-            curPrice += orderPrice[char];
+    for (let i = 1; i < words.length; i++) {
+        const word1 = words[i - 1];
+        const word2 = words[i];
+        const maxLen = Math.max(word1.length, word2.length);
+        let temp = 0;
+        while (temp < maxLen) {
+            const [o1, o2] = [dictonary[word1[temp]], dictonary[word2[temp]]];
+            if (o1 === undefined)
+                return false;
+            if (o1 === o2) {
+                temp++;
+                continue;
+            }
+            temp;
+            if (o1 > o2) {
+                return false;
+            }
+            else
+                break;
         }
-        if (curPrice < prevPrice) {
-            return false;
-        }
-        prevPrice = curPrice;
     }
     return true;
 }
 ;
 console.log(isAlienSorted(["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz"));
-console.log(isAlienSorted(["word", "world", "row"], "worldabcefghijkmnpqstuvxyz"));
+console.log(isAlienSorted(["app", "apple"], "abcdefghijklmnopqrstuvwxyz"));
+// console.log(isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz"));
+console.log(isAlienSorted(["kuvp", "q"], "ngxlkthsjuoqcpavbfdermiywz"));
+// console.log(isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz"));
--- tasks/leetcodePatterns/Easy/#63.VerifyingAlienDictionary/index.ts
+++ tasks/leetcodePatterns/Easy/#63.VerifyingAlienDictionary/index.ts
@@ -1,30 +1,40 @@
 //https://leetcode.com/problems/verifying-an-alien-dictionary/
 
 function isAlienSorted(words: string[], order: string): boolean {
-    const orderPrice: Record<string, number> = {}
+  if(words.length < 2) return true
 
-    for(let i = 0; i < order.length; i++){
-      orderPrice[order[i]] = i
-    }
+  const dictonary: Record<string, number> = {}
 
-    let prevPrice = 0
-    for(let word of words){
+  for(let i = 0; i < order.length; i++){
+    dictonary[order[i]] = i + 1
+  }
 
-      let curPrice = 0
-      for(let char of word){
-        curPrice += orderPrice[char]
-      }
+  for(let i = 1; i < words.length; i++){    
+    const word1 = words[i - 1]
+    const word2 = words[i]
+    
+    const maxLen = Math.max(word1.length, word2.length)
+    let temp = 0
 
-      if(curPrice < prevPrice){
+    while(temp < maxLen){
+      const [o1, o2] = [dictonary[word1[temp]], dictonary[word2[temp]]]
+      
+      if(o1 === o2){
+        temp++
+        continue
+      }temp
+      
+      if(!o2 || o1 > o2){
         return false
-      }
-
-      prevPrice = curPrice
+      } else break
     }
-
-    return true
+  }
+  
+  return true
 };
 
 console.log(isAlienSorted(["hello","leetcode"], "hlabcdefgijkmnopqrstuvwxyz"));
-console.log(isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz"));
+console.log(isAlienSorted(["app", "apple"], "abcdefghijklmnopqrstuvwxyz"));
+// console.log(isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz"));
 console.log(isAlienSorted(["kuvp","q"],"ngxlkthsjuoqcpavbfdermiywz"));
+// console.log(isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz"));
