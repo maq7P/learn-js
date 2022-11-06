ea6e5625caf3cff71f0217e9793d35fda8873ea2
maq7 <maxpet16@mail.ru>
2022-11-06 17:06:15 +0500

    [leetCode]: have been fixed bug with task of licenseKeyFromatting

--- tasks/leetcodePatterns/Easy/#64.LicenseKeyFormatting/index.ts
+++ tasks/leetcodePatterns/Easy/#64.LicenseKeyFormatting/index.ts
@@ -1,22 +1,17 @@
 //https://leetcode.com/problems/license-key-formatting/
 
 function licenseKeyFormatting(s: string, k: number): string {
-  const initS = s.toUpperCase().split("-");
-
   let res = ""
 
-  console.log(initS);
-  
-  for(let c of initS){
-    console.log(res.length);
-    
-    if(res.length > 0 && res.length % k === 0){
+  for(let char of s.split("-").join("").split("").reverse()){
+    if((res.length + 1) % (k + 1) === 0){
       res += "-"
     }
-    res += c
+    res += char.toUpperCase()
   }
 
-  return res
+  return res.split("").reverse().join("")
 };
 
 console.log(licenseKeyFormatting("5F3Z-2e-9-w", 4));
+console.log(licenseKeyFormatting("2-5g-3-J", 2));
