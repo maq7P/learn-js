0ac855ac2143e902923aaa93e5043a0cf0717f09
maq7 <maxpet16@mail.ru>
2022-11-13 15:26:09 +0500

    [leetCode]: resolved reverse vowels string

---
+++ tasks/leetcodePatterns/Easy/#72.ReverseVowelsString/index.js
@@ -0,0 +1,31 @@
+"use strict";
+//https://leetcode.com/problems/reverse-vowels-of-a-string/
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+function reverseVowels(s) {
+    let left = 0, right = s.length - 1;
+    const sArr = s.split("");
+    while (left <= right) {
+        const isCurLeftVowels = vowels.indexOf(sArr[left]) !== -1;
+        const isCurRightVowels = vowels.indexOf(sArr[right]) !== -1;
+        if (isCurLeftVowels && isCurRightVowels) {
+            [sArr[left], sArr[right]] = [sArr[right], sArr[left]];
+            console.log("sArr: ", sArr);
+            left++;
+            right--;
+            continue;
+        }
+        if (isCurLeftVowels) {
+            right--;
+            continue;
+        }
+        if (isCurRightVowels) {
+            left++;
+            continue;
+        }
+        left++;
+        right--;
+    }
+    return sArr.join("");
+}
+;
+console.log(reverseVowels("hello"));
---
+++ tasks/leetcodePatterns/Easy/#72.ReverseVowelsString/index.ts
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/reverse-vowels-of-a-string/
+
+const vowels = ['a', 'e', 'i', 'o', 'u', "A", "E", "I", "O", "U"];
+
+function reverseVowels(s: string): string {
+  let left = 0, right = s.length - 1
+  const sArr = s.split("");
+
+  while(left <= right){
+    const isCurLeftVowels = vowels.indexOf(sArr[left]) !== -1
+    const isCurRightVowels = vowels.indexOf(sArr[right]) !== -1
+    
+    
+    if(isCurLeftVowels && isCurRightVowels){
+      [sArr[left], sArr[right]] = [sArr[right], sArr[left]]
+      
+      left++
+      right--
+      continue
+    }
+
+    if(isCurLeftVowels){
+      right--
+      continue
+    }
+
+    if(isCurRightVowels){
+      left++
+      continue
+    }
+
+    left++
+    right--
+  }
+
+  return sArr.join("")
+};  
+
+console.log(reverseVowels("hello"));
+
+
