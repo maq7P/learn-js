fe39a299268e84eea5df68027ed53e52ac580582
maq7 <maxpet16@mail.ru>
2022-11-18 14:52:40 +0500

    [leetCode]: resolved length last world

---
+++ tasks/leetcodePatterns/Easy/#75.LengthLastWord/index.js
@@ -0,0 +1,22 @@
+"use strict";
+//https://leetcode.com/problems/length-of-last-word/
+function lengthOfLastWord(s) {
+    const splitWithSpaces = s.split(" ");
+    console.log(splitWithSpaces);
+    if (splitWithSpaces.length === 1 && splitWithSpaces[0]) {
+        return splitWithSpaces[0].length;
+    }
+    let temp = splitWithSpaces.length - 1;
+    while (temp > 0) {
+        const cur = splitWithSpaces[temp];
+        console.log(cur);
+        if (cur)
+            return cur.length;
+        temp--;
+    }
+    return 0;
+}
+;
+// console.log(lengthOfLastWord("Hello    World   "));
+// console.log(lengthOfLastWord("a"));
+console.log(lengthOfLastWord("a "));
---
+++ tasks/leetcodePatterns/Easy/#75.LengthLastWord/index.ts
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/length-of-last-word/
+
+function lengthOfLastWord(s: string): number {
+  const splitWithSpaces = s.split(" ")
+  
+  if(splitWithSpaces.length === 1 && splitWithSpaces[0]){
+    return splitWithSpaces[0].length
+  }
+
+  let temp = splitWithSpaces.length - 1
+
+  while(temp >= 0){
+    const cur = splitWithSpaces[temp]
+    
+    if(cur) return cur.length
+    temp--
+  }
+
+  return 0
+};
+
+// console.log(lengthOfLastWord("Hello    World   "));
+// console.log(lengthOfLastWord("a"));
+// console.log(lengthOfLastWord("a "));
+
+
