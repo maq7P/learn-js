82ff7d2c1476f156a30ce7bd1d96309cc7f6f97e
maq7 <maxpet16@mail.ru>
2022-11-19 18:21:29 +0500

    [leetCode]: resolved max consecutive ones

---
+++ tasks/leetcodePatterns/Easy/#77.MaxConsecutiveOnes/index.ts
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/max-consecutive-ones/
+
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let max = 0, cur = 0
+  
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] === 0){
+      cur = 0
+      continue
+    }
+
+    cur += nums[i]
+
+    if(cur > max){
+      max = cur
+    }
+  }
+
+  return max
+};
+
+findMaxConsecutiveOnes([1,1,0,1,1,1])
+findMaxConsecutiveOnes([1,0,1,1,0,1])
+findMaxConsecutiveOnes([1,1,0,1])
