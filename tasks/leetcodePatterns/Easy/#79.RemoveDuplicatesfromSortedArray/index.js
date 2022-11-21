3aaefb3145861db536a4f0c865d5514bbd0b24a8
maq7 <maxpet16@mail.ru>
2022-11-21 19:02:15 +0500

    [leetCode]: resolved remove duplicates from sorted arr

---
+++ tasks/leetcodePatterns/Easy/#79.RemoveDuplicatesfromSortedArray/index.js
@@ -0,0 +1,21 @@
+"use strict";
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+function removeDuplicatesSorted(nums) {
+    let fixPointer = 0, searchPointer = 0;
+    while (searchPointer < nums.length) {
+        if (nums[searchPointer] > nums[fixPointer]) {
+            fixPointer++;
+            [nums[searchPointer], nums[fixPointer]] = [nums[fixPointer], nums[searchPointer]];
+            searchPointer = fixPointer;
+            continue;
+        }
+        searchPointer++;
+    }
+    return ++fixPointer;
+}
+;
+//[0,0,1,1,1,2,2,3,3,4]
+const test1 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+console.log(test1);
+console.log(removeDuplicatesSorted(test1));
+console.log(test1);
