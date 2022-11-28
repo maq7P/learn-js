7814c52848f8bd18e5bc602039a07806aea494a6
maq7 <maxpet16@mail.ru>
2022-11-28 14:17:52 +0500

    updated solution of longest common prefix

--- tasks/leetcodePatterns/Easy/#22.LongestCommonPrefix/index.ts
+++ tasks/leetcodePatterns/Easy/#22.LongestCommonPrefix/index.ts
@@ -1,10 +1,23 @@
 //https://leetcode.com/problems/longest-common-prefix/
 
-//TODO: re
 function longestCommonPrefix(words: string[]): string {
   if (!words[0] || words.length == 1) return words[0] || "";
-  let i = 0;
-  while (words[0][i] && words.every(w => w[i] === words[0][i]))
-    i++;
-  return words[0].substr(0, i);
+  
+  let res = "";
+
+  for(let i = 0; i < words[0].length; i++){
+    const char = words[0][i]
+
+    for(let word of words){
+      if(word[i] !== char){
+        return res
+      }
+    }
+
+    res += char
+  }
+
+  return res
 };
+
+console.log(longestCommonPrefix(["flower","flower","flower","flower"]));
