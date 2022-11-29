6c71abff41f5aa7c3ee09e13cb9f83c826797d94
maq7 <maxpet16@mail.ru>
2022-11-29 23:50:18 +0500

    [leetCode]: resolved happy number

---
+++ tasks/leetcodePatterns/Easy/#84.HappyNumber/index.ts
@@ -0,0 +1,17 @@
+const isHappy = (n: any, hash = {}): any => {
+  if (n !== 1) {
+      if (hash[n]) {
+        return false
+      }
+
+      hash[n] = 1
+
+      const a = n.toString().split("");
+      const b = a.map(x => parseInt(x))
+      const sum = b.reduce((total, x) => total + x**2, 0)
+
+      return isHappy(sum, hash)
+  }
+
+  return true
+};
