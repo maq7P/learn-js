efa8add411563fd8fc7585ce5d6dc29044ebdd7c
maq7 <maxpet16@mail.ru>
2022-12-03 18:51:58 +0500

    [leetCode]: hammingWeight

---
+++ tasks/leetcodePatterns/Easy/#91.Numberof1Bits/index.ts
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/number-of-1-bits/
+
+function hammingWeight(n: number): number {
+  let ans = 0
+
+  for(let i = 0; i < 32; i++){
+      ans += 1 & n
+      n = n >> 1
+  }
+
+  return ans
+};
