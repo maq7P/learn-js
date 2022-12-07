d33d8ef6f3d5344d2a1d3acdfc9834e42b0545cf
maq7 <maxpet16@mail.ru>
2022-12-07 18:47:28 +0500

    [leetCode]: avg levels of BST

---
+++ tasks/leetcodePatterns/Easy/#96.AverageLevelsBinaryTree/index.js
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/average-of-levels-in-binary-tree/
+import TreeNode from "../../common/TreeNode/index.js";
+function averageOfLevels(root) {
+    const q = [root];
+    const res = [];
+    while (q.length) {
+        let count = 0, sum = 0;
+        const qLen = q.length;
+        for (let i = 0; i < qLen; i++) {
+            const cur = q.shift();
+            if (cur) {
+                count++;
+                sum += cur.val;
+                cur.left && q.push(cur.left);
+                cur.right && q.push(cur.right);
+            }
+        }
+        res.push(sum / count);
+    }
+    return res;
+}
+;
+console.log(averageOfLevels(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
