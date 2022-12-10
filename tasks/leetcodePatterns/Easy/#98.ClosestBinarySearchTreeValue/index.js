9de64c43917b5bbab9e700f11da1dd046e8e5fec
maq7 <maxpet16@mail.ru>
2022-12-10 16:25:33 +0500

    [leetCode]: closest bst

---
+++ tasks/leetcodePatterns/Easy/#98.ClosestBinarySearchTreeValue/index.js
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/closest-binary-search-tree-value
+import TreeNode from "../../common/TreeNode/index.js";
+const closestValue = (root, target) => {
+    const dfs = (node, cur) => {
+        if (!node || target === node.val)
+            return cur;
+        if (Math.abs(target - node.val) < Math.abs(cur - target)) {
+            cur = node.val;
+        }
+        const right = dfs(node.right, cur);
+        const left = dfs(node.left, cur);
+        return Math.abs(target - left) < Math.abs(target - right) ? left : right;
+    };
+    return dfs(root, +Infinity);
+};
+console.log(closestValue(new TreeNode(3, new TreeNode(2, new TreeNode(1), new TreeNode(4.5)), new TreeNode(3.6)), 4));
