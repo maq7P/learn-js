a4237ff8e26d7ff887eb871eaf92fca41ecded60
maq7 <maxpet16@mail.ru>
2022-12-28 18:26:30 +0500

    [leetCode]: resolved with dp

--- tasks/leetcodePatterns/Medium/#7.BestTimeBuyandSellStock/index.ts
+++ tasks/leetcodePatterns/Medium/#7.BestTimeBuyandSellStock/index.ts
@@ -1,38 +1,17 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-function isDecrese(nums: number[]): boolean {
-  let isDecrese = false
-
-  for(let i = 0; i < nums.length; i++){  
-    let [first, second] = [nums[i], nums[i + 1]]
-
-    if(first > second) (isDecrese = true)
-
-    if(first < second){
-      return false
-    }
-  }
-  
-  return true
-};
-
 function maxProfitPrices(prices: number[]): number {
 
-  let l = 0, r = prices.length - 1
-  let min = prices[l], max = prices[r];
+  let min = -Infinity, maxProfit = +Infinity;
 
-  let isDecresePrices = isDecrese(prices)
-  
-  while(l < r){
-    l++
-    r--
-    
-    prices[l] < min && (min = prices[l])
-    prices[r] > max && (max = prices[r])
+  for(let p of prices){
+    min = Math.min(min, p)
+    maxProfit = Math.max(maxProfit, p - min)
   }
   
-  return isDecresePrices ? 0 : max - min
+  return maxProfit
 };
+
 console.log(maxProfitPrices([7,1,5,3,6,4]));
 console.log(maxProfitPrices([7,6,4,3,1]));
 console.log(maxProfitPrices([1,2]));
