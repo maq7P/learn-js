d5448dabad4a540e838033928a20e082a37093d4
maq7 <maxpet16@mail.ru>
2022-12-30 18:21:55 +0500

    [leetCode]: binary gap work solution

--- tasks/leetcodePatterns/Easy/#110.BinaryGap/index.ts
+++ tasks/leetcodePatterns/Easy/#110.BinaryGap/index.ts
@@ -1,38 +1,24 @@
 //https://leetcode.com/problems/binary-gap/
 
-//TODO: not working, find error
 function binaryGap(n: number): number {
 
   const binary = n.toString(2).split("")
 
+  let gaps = []
   let maxGap = 0
 
   for(let i = 0; i < binary.length; i++){
-    let bit = binary[i]
+    const bit = binary[i]
+    
+    if(bit === "0") continue
 
-    if(bit === "1"){
-      i++
-      bit = binary[i]
-      let gap = 0
+    gaps.push(i)
+  }
 
-      while(bit === "0"){
-        if(i === binary.length - 1){
-          gap = 0
-          break;
-        }
-        console.log(bit);
-        
-        gap++
-        i++
-        bit = binary[i]
-      }
-      
-      console.log("gap: ", gap);
-      
-      if(gap > 0 && gap + 1 > maxGap){
-        maxGap = gap + 1
-      }
-    }
+  for(let i = 0; i < gaps.length - 1; i++){
+    const diff = gaps[i + 1] - gaps[i]
+
+    maxGap = Math.max(diff, maxGap)
   }
 
   return maxGap
